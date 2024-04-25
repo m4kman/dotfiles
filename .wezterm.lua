e89cc90498b2e5564c299bd7ed603ac2c4fee04e
M. Hassam Khan <30337413+m4kman@users.noreply.github.com>
2024-04-25 23:09:53 +0500

    add: wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,28 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Vacuous 2 (terminal.sexy)"
+
+-- Open WSL by default
+-- config.default_domain = 'WSL:Debian'
+config.default_prog = { "C:/Program Files/Git/bin/bash.exe" }
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.font = wezterm.font("Monaspace Neon")
+config.font_size = 13
+config.hide_tab_bar_if_only_one_tab = true
+config.window_padding = {
+	left = 0.75,
+	right = 0.5,
+	top = 0,
+	bottom = 0,
+}
+-- and finally, return the configuration to wezterm
+return config
