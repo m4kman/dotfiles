09b8930d9ecb3b93c3955425972e369be2c4fdea
M. Hassam Khan <30337413+m4kman@users.noreply.github.com>
2024-05-11 22:12:29 +0500

    update nvim colorscheme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "datsfilipe/vesper.nvim",
+    name = "vesper",
+  },
+  {
+    "rebelot/kanagawa.nvim",
+  },
+  {
+    "olivercederborg/poimandres.nvim",
+  },
+  {
+    "oxfist/night-owl.nvim",
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "night-owl",
+    },
+  },
+}
