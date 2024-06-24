dc1eb8ff8843eab551680b855ceed97ceb6c493a
M. Hassam Khan <30337413+m4kman@users.noreply.github.com>
2024-06-24 23:54:35 +0500

    add fzf-lua plugin

---
+++ nvim/lua/plugins/fzf.lua
@@ -0,0 +1,16 @@
+return {
+  "ibhagwan/fzf-lua",
+  opts = {
+    file_ignore_patterns = {
+      "node_modules",
+      ".next/",
+      "%.lockb",
+      "%.png",
+      "%.jpg",
+      "%.webp",
+      "%.gif",
+      "%-lock.json",
+      "%-lock.yaml",
+    },
+  },
+}
