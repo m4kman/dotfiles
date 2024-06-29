359fe3e7d850b68d7dda585935aaa414e18fd01d
M. Hassam Khan <30337413+m4kman@users.noreply.github.com>
2024-06-29 23:55:58 +0500

    add mason config

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,19 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "json-lsp",
+      "lua-language-server",
+      "markdown-toc",
+      "markdownlint",
+      "marksman",
+      "mdx-analyzer",
+      "prettier",
+      "prettierd",
+      "shfmt",
+      "stylua",
+      "tailwindcss-language-server",
+      "vtsls",
+    },
+  },
+}
